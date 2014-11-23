c879d251756f692671f59b3f70d7ec592130796b
cp803154 <snoopwarrior@hotmail.fr>
2014-11-23 19:49:09 +0100

    Test des mouvements

--- test/creatures/movement/MovementBouncingTest.java
+++ test/creatures/movement/MovementBouncingTest.java
@@ -1,15 +1,53 @@
 package creatures.movement;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import worlds.IWorld;
+import worlds.WorldClosed;
+import creatures.AbstractCreature;
+import creatures.StandardCreature;
+import creatures.visual.CreatureSimulator;
+
 public class MovementBouncingTest {
+	
+	CreatureSimulator environment = mock(CreatureSimulator.class);
+	final double w = 200;
+	final double h = 100;
+	AbstractCreature creature;
+	IWorld worldStrategy = new WorldClosed(environment);
+	IMovement movementStrat = new MovementBouncing(environment);
+	
+	@Before
+	public void setup() {
+		when(environment.getSize()).thenReturn(new Dimension((int)w, (int)h));
+		creature = new StandardCreature(environment, new Point2D.Double(0, 0), 0, 5, Color.RED, worldStrategy, movementStrat);
+	}
 
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
-		assertTrue(true);
+	public void directionChanged() {
+		double direction = creature.getDirection();
+		for(int i = 0; i < 30; i++) {
+			creature.act();
+		}
+		assertNotEquals(creature.getDirection(), direction, 0.001);;
+	}
+	
+	@Test
+	public void speedChanged() {
+		double speed = creature.getSpeed();
+		for(int i = 0; i < 30; i++) {
+			creature.act();
+		}
+		assertNotEquals(creature.getSpeed(), speed, 0.001);
 	}
 
 }
--- test/creatures/movement/MovementRandomTest.java
+++ test/creatures/movement/MovementRandomTest.java
@@ -1,14 +1,45 @@
 package creatures.movement;
 
-import static org.junit.Assert.assertTrue;
+import static java.lang.Math.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import worlds.IWorld;
+import worlds.WorldClosed;
+import creatures.AbstractCreature;
+import creatures.StandardCreature;
+import creatures.visual.CreatureSimulator;
+
 public class MovementRandomTest {
+	
+	CreatureSimulator environment = mock(CreatureSimulator.class);
+	final double w = 200;
+	final double h = 100;
+	AbstractCreature creature;
+	IWorld worldStrategy = new WorldClosed(environment);
+	IMovement movementStrat = new MovementRandom(environment);
+	
+	@Before
+	public void setup() {
+		when(environment.getSize()).thenReturn(new Dimension((int)w, (int)h));
+		creature = new StandardCreature(environment, new Point2D.Double(0, 0), 0, 5, Color.RED, worldStrategy, movementStrat);
+	}
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void move() {
+		creature.act();
+		
+		Assert.assertEquals(0, creature.getDirection(), 0.001);
+		Assert.assertEquals(creature.getSpeed() * cos(creature.getDirection()), creature.getPosition().getX(), 0.01);
+		Assert.assertEquals(creature.getSpeed() * sin(creature.getDirection()), creature.getPosition().getY(), 0.01);
 	}
 
 }
